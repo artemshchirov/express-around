1c8765f8f90ef334ba7b85f7541eb912c95aafeb
Artem Schirov <78075439+artemschirov@users.noreply.github.com>
2022-07-31 22:32:00 +0300

    fix autotest 2

--- src/controllers/cardControllers.js
+++ src/controllers/cardControllers.js
@@ -7,17 +7,21 @@ exports.getCards = async (req, res) => {
     const cards = await Card.find({});
     res.status(OK).send(cards);
   } catch (err) {
-    errorMessage(err, req, res);
+    errorMessage(err.name, req, res);
   }
 };
 
 exports.deleteCardById = async (req, res) => {
   const { cardId } = req.params;
   try {
-    const card = await Card.findByIdAndRemove(cardId);
+    const card = await Card.findByIdAndRemove(cardId).orFail(() => {
+      const e = new Error("404: card with _id not found");
+      e.name = "DocumentNotFoundError";
+      throw e;
+    });
     res.status(OK).send({ data: card });
   } catch (err) {
-    errorMessage(err, req, res);
+    errorMessage(err.name, req, res);
   }
 };
 
@@ -33,7 +37,7 @@ exports.createCard = async (req, res) => {
     newCard.populate("owner");
     res.status(CREATED).send(newCard);
   } catch (err) {
-    errorMessage(err, req, res);
+    errorMessage(err.name, req, res);
   }
 };
 
@@ -45,10 +49,14 @@ exports.likeCard = async (req, res) => {
       cardId,
       { $addToSet: { likes: _id } },
       { new: true }
-    );
+    ).orFail(() => {
+      const e = new Error("404: card with _id not found");
+      e.name = "DocumentNotFoundError";
+      throw e;
+    });
     res.status(OK).send({ data: card });
   } catch (err) {
-    errorMessage(err, req, res);
+    errorMessage(err.name, req, res);
   }
 };
 
@@ -60,9 +68,13 @@ exports.dislikeCard = async (req, res) => {
       cardId,
       { $pull: { likes: _id } },
       { new: true }
-    );
+    ).orFail(() => {
+      const e = new Error("404: card with _id not found");
+      e.name = "DocumentNotFoundError";
+      throw e;
+    });
     res.status(OK).send({ data: card });
   } catch (err) {
-    errorMessage(err, req, res);
+    errorMessage(err.name, req, res);
   }
 };
--- src/utils/errorMessage.js
+++ src/utils/errorMessage.js
@@ -5,7 +5,7 @@ const {
 } = require("./constants");
 
 exports.errorMessage = (err, req, res) => {
-  switch (err.name) {
+  switch (err) {
     case "CastError":
       res.status(BAD_REQUEST).send({
         message: "Переданы некорректные данные",
